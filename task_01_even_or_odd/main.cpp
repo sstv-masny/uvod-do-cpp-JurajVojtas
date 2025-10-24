76bbed00d77999307cd51c95c69bee350cf4ef8f
JurajVojtas <jurajvojtas59@gmail.com>
2025-10-24 08:15:53 +0200

    Update main.cpp

--- task_01_even_or_odd/main.cpp
+++ task_01_even_or_odd/main.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     long long n;
-    cout<<"Enter the number of terms you want to find: ";
-    cin>>n;
-    if (n%2==0)
-        cout<<"Even";
+    cout << "Enter the number of terms you want to find: ";
+    cin >> n;
+    if (n % 2 == 0)
+        cout << "Even";
     else
-        cout<<"Odd";
-
+        cout << "Odd";
 
     // TODO: print "even" or "odd"
     return 0;

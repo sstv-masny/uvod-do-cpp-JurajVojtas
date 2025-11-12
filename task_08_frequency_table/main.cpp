5a91f3d74386b0f54e56c9a4531aa43ae16074b5
JurajVojtas <jurajvojtas59@gmail.com>
2025-11-12 12:25:06 +0100

    ...

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -1,11 +1,9 @@
 
 #include <iostream>
-#include <vector>
 using namespace std;
 int main() {
-
-    int n;
-
+    int n[5] = {1, 2, 3, 4, 5};
+    cout << n[3];
     // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
     return 0;
 }

020a7cc3b9161a42501cccccad670f266e540011
RomanKramarcik <jurajvojtas59@gmail.com>
2025-11-18 09:30:36 +0100

    gfg

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -1,9 +1,14 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-    int n[5] = {1, 2, 3, 4, 5};
-    cout << n[3];
+    // int n[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    char number[5] = {'f', 'i', 'l', 'i', 'p'};
+    string meno = "filip";
+    for (int i = 0; i < 11; i++) {
+        cout << meno[i] << endl;
+    }
     // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
     return 0;
 }

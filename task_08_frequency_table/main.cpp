98c4f2df16c82f2e2cafe9bde56fb3e01636aa34
RomanKramarcik <jurajvojtas59@gmail.com>
2025-11-19 14:01:32 +0100

    Update main.cpp

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -3,24 +3,17 @@
 #include <string>
 using namespace std;
 int main() {
-<<<<<<< HEAD
-    // int n[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    char number[5] = {'f', 'i', 'l', 'i', 'p'};
-    string meno = "filip";
-    for (int i = 0; i < 11; i++) {
-        cout << meno[i] << endl;
-    }
-    // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
-=======
+    int n[101] = {};
 
-    int n[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7};
+    n[1] += 1;
+    n[2] += 1;
+    n[2] += 1;
+    n[100] += 1;
+    n[0] += 1;
 
-    cout << sizeof(n) / 4;
-
-    for (int i = 0; i < 12; i++) {
-        cout << n[i] << '\n';
+    for (int pocet : n) {
+        cout << pocet << " ";
     }
 
->>>>>>> c57e2686397020cf4ed5f8b16281ea5ff4a6a463
     return 0;
 }
